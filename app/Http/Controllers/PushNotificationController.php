d6018a137079979406e159583c2e16e73eeab3e9
Amine Admane <gm_admane@esi.dz>
2021-03-21 17:23:43 +0100

    some changes

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\PushNotification;
 use App\cr;
 use Illuminate\Http\Request;
@@ -10,17 +11,21 @@ class PushNotificationController extends Controller
 
     public function bulksend()
     {
-        $notification = [    'title' => 'This is notification title',    'body' =>'This is notification text',    
-        'alert' => 'Test Push Message',    'sound' => 'default'];
-        $data = [  'nom' => "Boutouili",  "prenom" => "Djillali" , "pickup" => "Babezzouar" , "dropoff" => "Oran","tel" => "0561403441" , "prix"=>"1500",
-          'priority' => 'high',"click_action" => "FLUTTER_NOTIFICATION_CLICK",    'content_available' => true];
-        
+        $notification = [
+            'title' => 'This is notification title',    'body' => 'This is notification text',
+            'alert' => 'Test Push Message',    'sound' => 'default'
+        ];
+        $data = [
+            'nom' => "Boutouili",  "prenom" => "Djillali", "pickup" => "Babezzouar", "dropoff" => "Oran", "tel" => "0561403441", "prix" => "1500",
+            'priority' => 'high', "click_action" => "FLUTTER_NOTIFICATION_CLICK",    'content_available' => true
+        ];
 
 
-        $fcmNotification = [    'to' => '/topics/all', 'notification' => $notification,    'data' => $data,    'priority' => 10];
-   
-        
-        $headers = array (
+
+        $fcmNotification = ['to' => '/topics/all', 'notification' => $notification,    'data' => $data,    'priority' => 10];
+
+
+        $headers = array(
             'Authorization: key=' . 'AAAAnXmso5w:APA91bEVFEWzsV71F8JPNQU86cC6eMe6z3tkK769tu5Ay0xjTybu0rkv9mIIE8fTrdsxJ6HzdxM1vTgPdfBlJfc7REwLSK4cJWp-blX-Judz2BM4C_anlX099zYvZtrWUiUMR6XA2ELU',
             'Content-Type: application/json'
         );
@@ -36,7 +41,6 @@ class PushNotificationController extends Controller
         $result = curl_exec($cRequest);
         curl_close($cRequest);
         echo $result;
-
     }
     public function bulksenf(Request $req)
     {
@@ -47,9 +51,6 @@ class PushNotificationController extends Controller
         $comment->body = $req->input('body');
         $comment->image = $req->input('image');
         $comment->save();
-
-
-    
     }
 
     /**
